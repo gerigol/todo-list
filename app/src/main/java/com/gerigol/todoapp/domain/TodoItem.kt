f930e93e34aea9ab170f184527301687813331d5
gerigol <golengerhard@gmail.com>
2023-08-09 15:39:09 +0200

    feat: create todo entity

---
+++ app/src/main/java/com/gerigol/todoapp/domain/TodoItem.kt
@@ -0,0 +1,21 @@
+package com.gerigol.todoapp.domain
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class TodoItem(
+    @ColumnInfo(name = "id")
+    @PrimaryKey(autoGenerate = true)
+    val id: Long = 0,
+
+    @ColumnInfo("title")
+    val title: String,
+
+    @ColumnInfo("description")
+    val description: String,
+
+    @ColumnInfo("is_checked")
+    val isChecked: Boolean = false
+)
