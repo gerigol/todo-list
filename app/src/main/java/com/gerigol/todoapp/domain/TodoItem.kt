f4afd475a240f4827469d77d27ce84931b21f81c
gerigol <golengerhard@gmail.com>
2023-08-09 15:53:18 +0200

    feat: add table name

--- app/src/main/java/com/gerigol/todoapp/domain/TodoItem.kt
+++ app/src/main/java/com/gerigol/todoapp/domain/TodoItem.kt
@@ -4,7 +4,7 @@ import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-@Entity
+@Entity(tableName = "todos")
 data class TodoItem(
     @ColumnInfo(name = "id")
     @PrimaryKey(autoGenerate = true)
