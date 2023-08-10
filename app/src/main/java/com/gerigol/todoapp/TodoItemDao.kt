957621e4f875b0d9c886c0ef092636925243f0b9
gerigol <golengerhard@gmail.com>
2023-08-10 09:36:14 +0200

    feat: create dao interface

---
+++ app/src/main/java/com/gerigol/todoapp/TodoItemDao.kt
@@ -0,0 +1,30 @@
+package com.gerigol.todoapp
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+
+import androidx.room.Update
+import com.gerigol.todoapp.domain.TodoItem
+
+
+@Dao
+interface TodoItemDao {
+    @Insert
+    fun addTodoItem(TodoItem: TodoItem?): Long
+
+    @Update
+    fun updateTodoItem(TodoItem: TodoItem?)
+
+    @Delete
+    fun deleteTodoItem(TodoItem: TodoItem?)
+
+
+    @Query("select * from todos")
+    fun getTodoItems(): List<TodoItem?>?
+
+    @Query("select * from todos where id ==:TodoItemId")
+    fun getTodoItem(TodoItemId: Long): TodoItem?
+
+}
